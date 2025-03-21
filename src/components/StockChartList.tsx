94f10aabdd4af54dace9e80fb36e9b8e97c004b1
Youn <tfcpa0716@gmail.com>
2025-03-21 22:30:39 +0900

    feat(dev) : Add sidebar component

--- src/components/StockChartList.tsx
+++ src/components/StockChartList.tsx
@@ -20,41 +20,45 @@ export default function StockChartList({
   };
 
   return (
-    <DragDropContext onDragEnd={handleDragEnd}>
-      <Droppable droppableId="droppable-1" direction="horizontal">
-        {(provided, snapshot) => (
-          <div
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-            style={{
-              backgroundColor: snapshot.isDraggingOver ? 'lightblue' : 'white',
-              padding: '10px',
-              borderRadius: '8px',
-            }}
-          >
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-              {items.map((item, index) => (
-                <Draggable key={item.id} draggableId={item.id} index={index}>
-                  {(provided) => (
-                    <div
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                    >
-                      <StockChart
-                        company={item.company}
-                        x1={item.x1}
-                        x2={item.x2}
-                      />
-                    </div>
-                  )}
-                </Draggable>
-              ))}
+    <div className="container mx-auto p-6">
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <Droppable droppableId="droppable-1" direction="horizontal">
+          {(provided, snapshot) => (
+            <div
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              style={{
+                backgroundColor: snapshot.isDraggingOver
+                  ? 'lightblue'
+                  : 'white',
+                padding: '10px',
+                borderRadius: '8px',
+              }}
+            >
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                {items.map((item, index) => (
+                  <Draggable key={item.id} draggableId={item.id} index={index}>
+                    {(provided) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        <StockChart
+                          company={item.company}
+                          x1={item.x1}
+                          x2={item.x2}
+                        />
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+              </div>
+              {provided.placeholder}
             </div>
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
-    </DragDropContext>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
   );
 }
